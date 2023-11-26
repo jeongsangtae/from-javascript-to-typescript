cb2ede06b64bb98e1e6a66b476bfe19a4b4e2f7b
jeongsangtae <wjdtkdxo55@naver.com>
2023-11-26 21:45:45 +0900

    From JavaScript To TypeScript - Explore basic types (basics.ts add)

---
+++ basics.ts
@@ -0,0 +1,17 @@
+// 기본형 (Primitives): number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "jeong";
+
+let isInstructor: boolean;
+
+isInstructor = true;
